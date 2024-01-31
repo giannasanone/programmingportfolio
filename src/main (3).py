dd2eb90343544cc8406078437204280fb9efb24d
giannasanone <158311394+giannasanone@users.noreply.github.com>
2024-01-31 10:14:31 -0700

    Add files via upload

---
+++ src/main (3).py
@@ -0,0 +1,12 @@
+#main.py
+# create a string called word and set it to "chrome"
+word = "pen"
+# create a int containing the count of word
+count = len(word)
+# change word to all lower case
+word = word.lower()
+# set first letter to a variable
+first = word[0]
+# set rest of word to a variable
+rest = word[1:len(word)]
+print(rest + first + "ay")
